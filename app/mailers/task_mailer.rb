e018026313f2fc825c70e8b3e7bb65c0e7c75250
shoponcom001 <shopon.com001@gmail.com>
2021-10-18 15:07:00 +0000

    Capter7-4

---
+++ app/mailers/task_mailer.rb
@@ -0,0 +1,12 @@
+class TaskMailer < ApplicationMailer
+
+    def creation_email(task)
+        @task = task
+        mail(
+            subject: "タスク作成完了メール",
+            to: "user@example.com",
+            from: "taskleaf@example.com"
+        )
+    end
+
+end
